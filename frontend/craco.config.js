369594eb6032dadbb82e1d97c8cf3d78363a28e1
Jhonny <jhonnydevb@gmail.com>
2025-08-07 13:48:48 -0300

    Normalize line endings to LF

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,7 +1,26 @@
+const tailwindcss = require('@tailwindcss/postcss');
+const autoprefixer = require('autoprefixer');
+const postcssNested = require('postcss-nested');
+const postcssCustomProperties = require('postcss-custom-properties');
+const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
+const postcssPresetEnv = require('postcss-preset-env');
+
 module.exports = {
   style: {
     postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+        postcssNested,
+        postcssCustomProperties,
+        postcssFlexbugsFixes,
+        postcssPresetEnv({
+          stage: 1,
+          features: {
+            'nesting-rules': false
+          }
+        }),
+      ],
     },
   },
 };
